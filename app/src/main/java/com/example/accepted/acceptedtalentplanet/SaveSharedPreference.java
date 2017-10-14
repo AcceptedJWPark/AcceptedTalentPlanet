59a5e110de403cb60013f3423557688893e28592
kwonhong <mkh9012@kookmin.ac.kr>
2017-10-14 17:37:14 +0900

    로그인 상태 관리

---
+++ app/src/main/java/com/example/accepted/acceptedtalentplanet/SaveSharedPreference.java
@@ -0,0 +1,48 @@
+package com.example.accepted.acceptedtalentplanet;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+/**
+ * Created by kwonhong on 2017-10-14.
+ */
+
+public class SaveSharedPreference {
+    static final String PREF_USER_NAME = "username";
+    static final String PREF_USER_ID = "userid";
+    static final String SERVER_IP = "http://13.124.141.242/Accepted/";
+    static final String SERVER_IP2 = "http://192.168.123.3:8080/Accepted/";
+    static SharedPreferences getSharedPreferences(Context ctx){
+        return PreferenceManager.getDefaultSharedPreferences(ctx);
+    }
+
+    public static void setPrefUsrName(Context ctx, String userName){
+        SharedPreferences.Editor editor = getSharedPreferences(ctx).edit();
+        editor.putString(PREF_USER_NAME, userName);
+        editor.commit();
+    }
+
+    public static void setPrefUsrId(Context ctx, String useId){
+        SharedPreferences.Editor editor = getSharedPreferences(ctx).edit();
+        editor.putString(PREF_USER_ID, useId);
+        editor.commit();
+    }
+    public static String getUserName(Context ctx){
+        return getSharedPreferences(ctx).getString(PREF_USER_NAME, "");
+    }
+
+    public static String getUserId(Context ctx){
+        return getSharedPreferences(ctx).getString(PREF_USER_ID, "");
+    }
+
+    public static void clearUserInfo(Context ctx){
+        SharedPreferences.Editor editor = getSharedPreferences(ctx).edit();
+        editor.clear();
+        editor.commit();
+    }
+
+    public static String getServerIp(){
+        return SERVER_IP;
+    }
+}
